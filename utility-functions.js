5053168e0bf57460d32d381f082d08c1188fea18
powersootbrite <sootpower3@gmail.com>
2024-09-20 09:49:36 +0000

    😘👩‍👩‍👦 Checkpoint ./utility-functions.js:69253235/1325

--- utility-functions.js
+++ utility-functions.js
@@ -1,11 +1,13 @@
+
 function convertCard(cardObject) {
-  let cardString = '<div class="card">';
-  cardString += '<img class="card-image-top" src="' + cardObject.src + '">';
-  cardString += '<div class="card-body">';
-  cardString += '<h3 class="card-title">' + cardObject.title + "</h3>";
-  cardString += cardObject.description;
-  cardString += "</div>";
-  cardString += "</div>";
+  //TEMPLATE STRING - SEE strings.html FOR MORE INFO
+  let cardString = 
+   `<div class="card">;
+      <img class="card-img-top" src="${cardObject.src}"/>
+      <div class="card-body">
+        <h3 class="card-title">${cardObject.title}</h3>
+        ${cardObject.description}
+      </div>
+    </div>`;
   return cardString;
 }
- 
