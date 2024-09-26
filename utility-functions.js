ccef9a2e5f20013d717e1f9eb9fa38eec200b363
powersootbrite <sootpower3@gmail.com>
2024-09-26 22:22:31 +0000

    🏤👛 Checkpoint ./utility-functions.js:69253235/706 ./about.html:69253235/108 ./contact.html:69253235/29

--- utility-functions.js
+++ utility-functions.js
@@ -1,11 +1,34 @@
+// function convertCard(cardObject) {
+//   let cardString = '<div class="card">';
+//   cardString += '<img class="card-image-top" src="' + cardObject.src + '">';
+//   cardString += '<div class="card-body">';
+//   cardString += '<h3 class="card-title">' + cardObject.title + "</h3>";
+//   cardString += cardObject.description;
+//   cardString += "</div>";
+//   cardString += "</div>";
+//   return cardString;
+// }
+
+
+  // const cards = [card1, card2, card3];
+        // for (position = 0; position < cards.length; position++) {
+        //   const card = cards[position];
+        //   const cardString = convertCard(card);
+        //   mainTag.innerHTML = mainTag.innerHTML + cardString;
+        // }
+ 
 function convertCard(cardObject) {
-  let cardString = '<div class="card">';
-  cardString += '<img class="card-image-top" src="' + cardObject.src + '">';
-  cardString += '<div class="card-body">';
-  cardString += '<h3 class="card-title">' + cardObject.title + "</h3>";
-  cardString += cardObject.description;
-  cardString += "</div>";
-  cardString += "</div>";
+  const { src = '', title = 'No Title', description = 'No Description' } = cardObject;
+
+  const cardString = `
+    <div class="card">
+      <img class="card-image-top" src="${src}" alt="${title}">
+      <div class="card-body">
+        <h3 class="card-title">${title}</h3>
+        <p>${description}</p>
+      </div>
+    </div>
+  `;
+
   return cardString;
 }
- 
