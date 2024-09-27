619d3945c75bca3be6321b1f98f689a4bf24a301
powersootbrite <sootpower3@gmail.com>
2024-09-27 01:26:38 +0000

    🐗💳 Checkpoint ./utility-functions.js:69253235/405 ./contact.html:69253235/4

--- utility-functions.js
+++ utility-functions.js
@@ -1,13 +1,13 @@
-// function convertCard(cardObject) {
-//   let cardString = '<div class="card">';
-//   cardString += '<img class="card-image-top" src="' + cardObject.src + '">';
-//   cardString += '<div class="card-body">';
-//   cardString += '<h3 class="card-title">' + cardObject.title + "</h3>";
-//   cardString += cardObject.description;
-//   cardString += "</div>";
-//   cardString += "</div>";
-//   return cardString;
-// }
+function convertCard(cardObject) {
+  let cardString = '<div class="card">';
+  cardString += '<img class="card-image-top" src="' + cardObject.src + '">';
+  cardString += '<div class="card-body">';
+  cardString += '<h3 class="card-title">' + cardObject.title + "</h3>";
+  cardString += cardObject.description;
+  cardString += "</div>";
+  cardString += "</div>";
+  return cardString;
+}
 
 
   // const cards = [card1, card2, card3];
@@ -37,18 +37,3 @@
 
 
 
-function convertCard(cardObject) {
-  const { src = '', title = 'No Title', description = 'No Description' } = cardObject;
-
-  const cardString = `
-    <div class="card">
-      <img class="card-image-top" src="${src}">
-      <div class="card-body">
-        <h3 class="card-title">${title}</h3>
-        <p>${description}</p>
-      </div>
-    </div>
-  `;
-
-  return cardString;
-}
